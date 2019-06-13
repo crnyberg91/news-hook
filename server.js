6ed2d5764a63354ddb94936694a0283076d7de8c
Chris Nyberg <chrisn83@gmail.com>
2019-06-12 22:06:12 -0400

    init files, scrape fn created

---
+++ server.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+const db = require("./models");
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost/fishHook", { useNewUrlParser: true });
+
+app.get("/scrape", function(req, res) {
+  axios.get("https://www.thefisherman.com/index.cfm?fuseaction=category.display&category_id=19").then(function(response) {
+    const $ = cheerio.load(response.data);
+    $(".col-lg-7").each(function(i, element) {
+      const result = {};
+      result.title = $(this)
+        .children(".articleText")
+        .text();
+      result.link = $(this)
+        .children("h3")
+        .children("a")
+        .attr("href");
+        console.log(result);
+      db.Article.create(result)
+        .then(function(dbArticle) {
+          console.log(dbArticle);
+        })
+        .catch(function(err) {
+          console.log(err);
+        });
+    });
+    res.send("Scrape Complete");
+  });
+});
+
+app.get("/articles", function(req, res) {
+  db.Article.find({})
+    .then(function(dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
+
+app.get("/articles/:id", function(req, res) {
+  db.Article.findOne({ _id: req.params.id })
+    .populate("note")
+    .then(function(dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
+
+app.post("/articles/:id", function(req, res) {
+  db.Note.create(req.body)
+    .then(function(dbNote) {
+      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+    })
+    .then(function(dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
+
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT);
+});

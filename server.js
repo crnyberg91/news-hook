8925d7c66d0302659b3298e87cc792d88d8406cd
Chris Nyberg <chrisn83@gmail.com>
2019-06-26 21:02:30 -0400

    added mongodb_uri to server

--- server.js
+++ server.js
@@ -14,7 +14,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/fishHook", { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
 
 app.get("/scrape", function(req, res) {
   axios.get("https://www.thefisherman.com/index.cfm?fuseaction=category.display&category_id=19").then(function(response) {
